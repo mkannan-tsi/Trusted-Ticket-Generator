cc01eb941dd9399ff8c9b6e31465f29da57c4326
mkannan-tsi <mkannan@tableau.com>
2017-09-03 15:18:27 +0800

    Create trusted.php

---
+++ trusted.php
@@ -0,0 +1,29 @@
+<?php
+
+$url = $_GET ['server'];
+$url = $url . "/trusted";
+$username = $_GET ['username'];
+$site = $_GET ['target_site'];
+
+if (isset($site) === TRUE)
+{
+	$data = array('username' => $username);
+}
+else
+{
+	$data = array('username' => $username, 'target_site' => $site);
+}
+
+$options = array(
+    'http' => array(
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+echo($result);
+if ($result === FALSE) { echo "-1"; }
+
+?>
